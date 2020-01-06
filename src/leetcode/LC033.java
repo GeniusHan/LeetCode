7b662cc26e6965a3abdb97dd8fd8b0bbe3d59b52
GeniusHan <985898043@qq.com>
2020-01-06 15:12:28 +0800

    #add

--- src/leetcode/LC033.java
+++ src/leetcode/LC033.java
@@ -2,33 +2,34 @@ package leetcode;
 
 public class LC033 {
 
-	
-	public int search(int[] nums, int target) 
-	{
-		int index = -1;
-		
-		int len = nums.length;
-		// ¶ş·Ö
-		int low = 0;
-		int hight = len - 1;
-		while(low < hight)
-		{
-			int middle = (low + hight)/2;
-			int compareNum = nums[middle];
-			if(target < compareNum)
-			{
-				hight = middle-1;
-			}
-			else if(target > compareNum)
-			{
-				low = middle+1;
-			}
-			else
-			{
-				index = middle;
-				break;
-			}
-		}
-		return index;
+	 public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+        int mid = left + (right-left)/2;
+
+        while(left <= right){
+            if(nums[mid] == target){
+                return mid;
+            }
+
+            if(nums[left] <= nums[mid]){  //×ó±ßÉıĞò
+                if(target >= nums[left] && target <= nums[mid]){//ÔÚ×ó±ß·¶Î§ÄÚ
+                    right = mid-1;
+                }else{//Ö»ÄÜ´ÓÓÒ±ßÕÒ
+                    left = mid+1;
+                }
+
+            }else{ //ÓÒ±ßÉıĞò
+                if(target >= nums[mid] && target <= nums[right]){//ÔÚÓÒ±ß·¶Î§ÄÚ
+                    left = mid +1;
+                }else{//Ö»ÄÜ´Ó×ó±ßÕÒ
+                    right = mid-1;
+                }
+
+            }
+            mid = left + (right-left)/2;
+        }
+
+        return -1;  //Ã»ÕÒµ½
     }
 }
---
+++ src/leetcode/LC034.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+/*
+    *  给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+ */
+public class LC034 {
+    public int[] searchRange(int[] nums, int target) {
+    	if(nums==null || nums.length==0) return new int[] {-1, -1};
+        
+        int targetPosition = binarySearch(nums, target);
+        if(targetPosition==-1) {
+        	return new int[] {-1, -1};
+        }
+        int start = targetPosition, end = targetPosition;
+        while(nums[start]==nums[start-1]) {
+        	start--;
+        }
+        while(nums[end]==nums[end+1]) {
+        	end++;
+        }
+        return new int[] {start, end};
+    }
+    
+    private int binarySearch(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+        while(left<=right) {
+        	int mid = left + (right-left)/2;
+        	if(nums[mid]==target) {
+        		//
+        		return mid;
+        	} else if(nums[mid]>target) {
+        		right = mid-1;
+        	} else {
+        		left = mid+1;
+        	}
+        }
+        return -1;
+    }
+}
